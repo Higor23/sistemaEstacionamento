b53ec641ec40097540d60609352e6d7f514e11f3
Higor23 <higor.lemospr@gmail.com>
2021-01-06 11:56:16 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,7 @@
+(function() {
+
+    document.querySelector('#send').addEventListener("click", e => {
+        const name = document.querySelector('#name').value;
+        const licence = document.querySelector('#licence').value;
+    });
+})();
